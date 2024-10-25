6eb24221bb63b71feb1def443b4c5564c76a587e
Dursun Murat <murat.ertunc456@outlook.com>
2024-10-25 09:26:57 +0300

    düzeltme

--- src/main/java/com/dme/DormitoryProject/entity/Roles.java
+++ src/main/java/com/dme/DormitoryProject/entity/Roles.java
@@ -2,16 +2,42 @@ package com.dme.DormitoryProject.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
 @Table(name = "roles")
 public class Roles {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     @ManyToMany(mappedBy = "roles")
-    private Set<User> users = new HashSet<>();
+    private List<User> users = new ArrayList<>();
 }
--- src/main/java/com/dme/DormitoryProject/entity/User.java
+++ src/main/java/com/dme/DormitoryProject/entity/User.java
@@ -2,7 +2,9 @@ package com.dme.DormitoryProject.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -11,7 +13,11 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String userName;
+
     private String mail;
+
     private String password;
 
     @ManyToMany
@@ -21,5 +27,45 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "roles_id")
     )
     //@JsonManagedReference
-    private Set<Roles> roles = new HashSet<>();
+    private List<Roles> roles = new ArrayList<>();
+
+    public List<Roles> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Roles> roles) {
+        this.roles = roles;
+    }
+
+    public void setUsernName(String usernName) {
+        this.userName = usernName;
+    }
+
+    public String getUsernName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
