3f6782c0c351ba90280c9d8107ea845b02b01251
Dursun Murat <murat.ertunc456@outlook.com>
2024-11-19 09:51:36 +0300

    .

--- src/main/java/com/dme/DormitoryProject/entity/EntryExit.java
+++ src/main/java/com/dme/DormitoryProject/entity/EntryExit.java
@@ -1,4 +1,36 @@
 package com.dme.DormitoryProject.entity;
 
+import com.dme.DormitoryProject.enums.EntryExit.EntryOrExit;
+import jakarta.persistence.*;
+
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "EntryExitTbl")
 public class EntryExit extends BaseEntity{
+    @Enumerated(EnumType.STRING)
+    private EntryOrExit entryExit;
+    @ManyToOne
+    @JoinColumn(name = "studentId")
+    private Student student;
+    private LocalTime momentaryTime = LocalTime.now();
+
+    public EntryOrExit getEntryExit() {
+        return entryExit;
+    }
+    public void setEntryExit(EntryOrExit entryExit) {
+        this.entryExit = entryExit;
+    }
+    public Student getStudent() {
+        return student;
+    }
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+    public LocalTime getMomentaryTime() {
+        return momentaryTime;
+    }
+    public void setMomentaryTime(LocalTime momentaryTime) {
+        this.momentaryTime = momentaryTime;
+    }
 }
---
+++ src/main/java/com/dme/DormitoryProject/enums/EntryExit/EntryOrExit.java
@@ -0,0 +1,6 @@
+package com.dme.DormitoryProject.enums.EntryExit;
+
+public enum EntryOrExit {
+    Entry,
+    Exit
+}
