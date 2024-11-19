ebc37029a757fd1ecb1d0cd74c79bd8b77df9ad9
Dursun Murat <murat.ertunc456@outlook.com>
2024-11-19 10:43:40 +0300

    .

--- src/main/java/com/dme/DormitoryProject/repository/IMailDao.java
+++ src/main/java/com/dme/DormitoryProject/repository/IMailDao.java
@@ -3,5 +3,5 @@ package com.dme.DormitoryProject.repository;
 import com.dme.DormitoryProject.entity.Mail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IMailDao extends JpaRepository<Long, Mail> {
+public interface IMailDao extends JpaRepository<Mail, Long> {
 }
