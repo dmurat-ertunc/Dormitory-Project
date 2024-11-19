2a4727c873bf860831bf141ea2d1aabf085293e4
Dursun Murat <murat.ertunc456@outlook.com>
2024-11-19 10:01:47 +0300

    .

--- src/main/java/com/dme/DormitoryProject/entity/Title.java
+++ src/main/java/com/dme/DormitoryProject/entity/Title.java
@@ -11,20 +11,26 @@ public class Title extends BaseEntity{
     private String name;
     @OneToMany(mappedBy = "title", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Staff> staffList;
+    @OneToMany(mappedBy = "title", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<PersonnelRequestForm> personnelRequestForms;
+
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public List<Staff> getStaffList() {
         return staffList;
     }
-
     public void setStaffList(List<Staff> staffList) {
         this.staffList = staffList;
+    }
+    public List<PersonnelRequestForm> getPersonnelRequestForms() {
+        return personnelRequestForms;
+    }
+    public void setPersonnelRequestForms(List<PersonnelRequestForm> personnelRequestForms) {
+        this.personnelRequestForms = personnelRequestForms;
     }
 }
